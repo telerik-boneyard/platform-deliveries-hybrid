066491b62490fd75ca8bdb099367d14fb6c838c7
Anton Dobrev <anton.dobrev@telerik.com>
2016-01-18 16:32:47 +0000

    Config options.

--- backend-services-offline-hybrid-sample/config.js
+++ backend-services-offline-hybrid-sample/config.js
@@ -2,6 +2,6 @@
 //WARNING: You must never include your master key in your app. We only use it
 //in order to initialize the required server-side items for the demo to function.
 Config = {
-    AppId: 'kql1xd2i82hi72wq',
-    MasterKey: '7AgcXtkVipo0q04i3sD0JKCnoDWd31IT' 
+    AppId: 'YOUR_APP_ID',
+    MasterKey: 'YOUR_APP_MASTER_KEY' 
 };
